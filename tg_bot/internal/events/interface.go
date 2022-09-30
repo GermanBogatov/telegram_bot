bad5e8abf8009745e237d8ba0a6935383bced4cd
GermanBogatov <Bogat1weee@mail.ru>
2022-09-30 21:29:24 +0300

    [add] refresh-token [update] project

--- tg_bot/internal/events/interface.go
+++
@@ -1,7 +0,0 @@
-package events
-
-import "github.com/GermanBogatov/tg_bot/internal/events/model"
-
-type ProcessEventStrategy interface {
-	Process(eventBody []byte) (model.ProcesedEvent, error)
-}
--- youtube_service/internal/events/model/request/model.go
+++
@@ -1,6 +0,0 @@
-package request
-
-type SearchTrack struct {
-	RequestID string `json:"request_id"`
-	Name      string `json:"name"`
-}
